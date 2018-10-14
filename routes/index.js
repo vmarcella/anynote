b691b751c794e6e31bd429e1769012c7501a4022
C3NZ <cenz@cenz.io>
2018-10-14 00:34:09 -0700

    Fixed creating a new note when a section is created with axios

--- routes/index.js
+++ routes/index.js
@@ -8,17 +8,18 @@ const Note = require('../models/note')
 router.get('/', function(req, res, next) {
     Section.find().then(sections => {
         sectionsWithNotes = {}
-   
+        console.log(sections)
         for (section in sections){
             sections[section].notes = []
+            console.log(sections[section])
             Note.find({sectionId: sections[section]._id}).then(notes => {
                 sections[section].notes = notes;
-                console.log(sections[section].notes)
             }).catch(err => {
                 console.log(err);
             });
         }
-             res.render('index', { title: 'anynote', sections: sections});
+            console.log(sections)
+             res.render('index', { title: 'anynote', sections: sections, notes: sections.notes});
     }).catch(err => {
         console.log(error);
     });
